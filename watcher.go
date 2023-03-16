73eff7a2b541a8f8e066f910439bfc4b1c38d0fb
Omer Mohamed Ali <envomer@users.noreply.github.com>
2023-03-16 22:16:40 +0400

    Remove logging on stat error

--- watcher.go
+++ watcher.go
@@ -129,7 +129,7 @@ func (w *Watcher) isFileEligibleForChange(path string) bool {
 
 	info, err := os.Stat(path)
 	if err != nil {
-		w.Logger.Error("Is file eligible: %s", err)
+		//w.Logger.Error("Is file eligible: %s", err)
 		return false
 	}
 
